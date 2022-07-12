0aeb2a9c082924e96d781e4f6e28aa83d9ba2c67
Eduard Martinez <martinez.bernal@gmail.com>
2022-07-13 00:27:22 +0200

    clases

---
+++ filter/filter.php
@@ -0,0 +1,125 @@
+<?php 
+
+namespace Ebookmode\Filter;
+use DOMDocument;
+use DOMXPath;
+use DomNode;
+use LIBXML_NOERROR;
+
+class Filter {
+
+	private DOMDocument $doc;
+	private DOMXPath $xpath;
+	private const IGNORED_ELEMENTS = [
+		'xml', '#comment', 'script', 'svg', 'iframe', 'object', 'form',
+		'button', 'input', 'select',  'textarea', 'footer', 'head'
+	];
+
+	private const BLOCK_ELEMENTS = [ 'blockquote', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'hr', 'div', 'ul', 'ol'];
+	
+	private $blockElementsXpath;
+
+	public function __construct(string $source) {
+		$this->doc = new DOMDocument();
+		$this->doc->loadHTML($source, LIBXML_NOERROR);
+		$this->xpath = new DOMXPath($this->doc);
+		$this->blockElementsXpath =  ".//".implode("|.//", self::BLOCK_ELEMENTS);
+	}
+
+
+	public function __invoke(): string
+	{
+		return $this->traverse($this->doc->documentElement);
+	}
+
+	private function hasTextOrImage(DomNode $element): bool 
+	{
+		return strlen(trim($element->nodeValue)) > 2 || $this->xpath->query(".//img", $element)->count() > 0;
+	}
+
+	private function isHidden(DomNode $element): bool
+	{
+		return 
+		    $element instanceof DomElement && (
+		      trim($element->getAttribute("aria-hidden")) === 'true' ||
+		      stripos($element->getAttribute("class"), 'hidden') !== false ||
+		      $element->hasAttribute('hidden')
+		    );
+	}
+
+	private function traverseChildren(DomNode $element): string
+	{
+	  return implode(
+	  	array_map(
+	  		fn($child) => $this->traverse($child), 
+	  		iterator_to_array($element->childNodes)
+	  	)
+	  );
+	}
+
+	private function traverse(DomNode $element): string
+	{
+		if(in_array($element->nodeName, self::IGNORED_ELEMENTS) || $this->isHidden($element)) {
+			return '';
+		}
+
+		switch($element->nodeName) {
+			case 'article':
+			case 'main':
+			case 'section':
+			case 'sup':
+			case 'sub':
+			case 'strong':
+			case 'time':
+			case 'var':
+			case 'i':
+			case 'b':
+			case 'u':
+			case 'address':
+			case 'blockquote':
+			case 'h1':
+			case 'h2':
+			case 'h3':
+			case 'h4':
+			case 'h5':
+			case 'h6':
+			case 'p':
+			case 'hr':
+			case 'li':
+			case 'ol':
+			case 'ul':
+			case 'article':
+				if(!$this->hasTextOrImage($element)) {
+					return '';
+				}      
+				return in_array($element->nodeName, self::BLOCK_ELEMENTS)?
+				  sprintf('<%s style="clear:both">%s</%s>', $element->nodeName, $this->traverseChildren($element), $element->nodeName):
+				  sprintf('<%s>%s</%s>', $element->nodeName, $this->traverseChildren($element), $element->nodeName);
+			case 'html':
+			case 'body':
+			case 'span':
+				return $this->traverseChildren($element);
+			case '#text':
+				return trim($element->nodeValue)? $element->nodeValue : '';
+			case 'a':
+				$href = trim($element->getAttribute('href'));
+				return $href && strpos($href, '#') !== 0 ?
+					sprintf('<a href="%s">%s</a>', $href, $this->traverseChildren($element)) : '';
+			case 'div':
+				if(!$this->hasTextOrImage($element)) {
+					return '';
+				}
+				if ($this->xpath->query($this->blockElementsXpath, $element)->count()) {
+					return $this->traverseChildren($element);        
+				}
+				return sprintf('<hr><div style="margin-bottom:20px">%s</div>',  $this->traverseChildren($element));
+			case 'img':
+				return sprintf('<img style="float:right" src="%s">', $element->getAttribute('src'));
+			default:
+				if($text = trim($element->nodeValue)) {
+					return  implode(["[$element->nodeName]\n", $this->traverseChildren($element)]);
+				}
+				return '';
+		}
+	}
+}
--- html/index.php
+++ html/index.php
@@ -1,117 +1,20 @@
 <?php 
 
-$blockElements = [ 'blockquote', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'hr', 'div', 'ul', 'ol'];
-$blockElementsXpath = ".//".implode("|.//", $blockElements);
+use EbookMode\Filter\Filter;
 
-$url = $_GET['url'] ?? "https://www.meneame.net";
+$url = $_GET['url'] ?? "https://www.elperiodico.com";
 
-$doc = new DOMDocument();
-$doc->loadHTMLFile($url, LIBXML_NOERROR);
-$xpath = new DOMXPath($doc);
+require_once('../filter/filter.php');
 
-function hasTextOrImage(DomNode $element): bool 
-{
-  global $xpath;
-  return strlen(trim($element->nodeValue)) > 2 || $xpath->query(".//img", $element)->count() > 0;
-}
+$filter = new Filter(
+  file_get_contents(
+    $url,
+    false,
+    stream_context_create([
+      'http' => ['user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? 'EbookMode' ]
+    ])
+  )
+);
 
-function traverseChildren(DomNode $element): string
-{
-  return implode(array_map(fn($child) => traverse($child), iterator_to_array($element->childNodes)));
-}
-
-function isHidden(DomNode $element): bool
-{
-  return 
-    $element instanceof DomElement && (
-      trim($element->getAttribute("aria-hidden")) === 'true' ||
-      stripos($element->getAttribute("class"), 'hidden') !== false ||
-      $element->hasAttribute('hidden')
-    );
-}
-
-function traverse(DomNode $element): string {
-  global $blockElements;
-  global $blockElementsXpath;
-  global $xpath;
-  
-  if(isHidden($element)) {
-    return '';
-  }
-
-  switch($element->nodeName) {
-    case 'xml':
-    case '#comment':
-    case 'script':
-    case 'svg':
-    case 'iframe':
-    case 'object':
-    case 'form':
-    case 'button':
-    case 'input':
-    case 'select':
-    case 'textarea':
-    case 'footer':
-    case 'head':
-      break;
-    case 'article':
-    case 'main':
-    case 'section':
-    case 'sup':
-    case 'sub':
-    case 'strong':
-    case 'time':
-    case 'var':
-    case 'i':
-    case 'b':
-    case 'u':
-    case 'address':
-    case 'blockquote':
-    case 'h1':
-    case 'h2':
-    case 'h3':
-    case 'h4':
-    case 'h5':
-    case 'h6':
-    case 'p':
-    case 'hr':
-    case 'li':
-    case 'ol':
-    case 'ul':
-    case 'article':
-      if(!hasTextOrImage($element)) {
-        return '';
-      }      
-      return in_array($element->nodeName, $blockElements)?
-          sprintf('<%s style="clear:both">%s</%s>', $element->nodeName, traverseChildren($element), $element->nodeName):
-          sprintf('<%s>%s</%s>', $element->nodeName, traverseChildren($element), $element->nodeName);
-    case 'html':
-    case 'body':
-    case 'span':
-      return traverseChildren($element);
-    case '#text':
-      return trim($element->nodeValue)? $element->nodeValue : '';
-    case 'a':
-      $href = trim($element->getAttribute('href'));
-      return $href && strpos($href, '#') !== 0 ?
-        sprintf('<a href="%s">%s</a>', $href, traverseChildren($element)) : '';
-    case 'div':
-      if(!hasTextOrImage($element)) {
-        return '';
-      }
-      if ($xpath->query($blockElementsXpath, $element)->count()) {
-        return traverseChildren($element);        
-      }
-      return sprintf('<hr><div style="margin-bottom:20px">%s</div>',  traverseChildren($element));
-    case 'img':
-      return sprintf('<img style="float:right" src="%s">', $element->getAttribute('src'));
-    default:
-      if($text = trim($element->nodeValue)) {
-        return  implode(["[$element->nodeName]\n", traverseChildren($element)]);
-      }
-  }
-  return '';
-}
-
-$content = traverse($doc->documentElement);
+$content = $filter();
 require("../templates/base.php");
